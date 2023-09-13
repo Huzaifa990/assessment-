3f4a4cf802d62c97d419d498f126f1714565d993
Huzaifa990 <sultanhuzaifa0@gmail.com>
2023-09-13 15:53:46 +0500

    first commit

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const indexRouter = require('./routes/index');
 const tradesRouter = require('./routes/trades');
 
 const app = express();
+app.use(express.json());
+const port = 8080;
 
 // view engine setup
 app.set('view engine', 'jade');
@@ -17,7 +19,108 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var Trade = require("./models/trades");
+
+app.get("/", (req, res)=>{
+    res.send("Running");
+})
+
 app.use('/trades', tradesRouter);
 app.use('/', indexRouter);
 
+app.post("/trades", async (req, res)=>{
+    try{
+        const trade = await Trade.create(req.body);
+        res.status(201).send(trade);
+    }
+    catch{
+        res.status(404).send("An error occured!");
+    }
+    
+})
+
+app.get("/trades", async (req, res)=>{
+    try{
+        var findUser = await Trade.findAll();
+        res.status(200).send(findUser);
+    }
+    catch{
+        res.status(404).send("Something went wrong!");
+    }
+})
+
+app.get("/trades/:id", async (req, res)=>{
+    try{
+        var id = req.params.id;
+        var findUser = await Trade.findOne({id: id})
+        res.status(200).send(findUser);
+    }
+    catch{
+        res.status(404).send("Something went wrong!");
+    }
+})
+
+app.put("/trades/:id", async (req, res)=>{
+    try{
+        var id = req.params.id;
+        var updateTrade = await Trade.update({
+            type: req.body.type,
+            user_id: req.body.user_id,
+            symbol: req.body.symbol,
+            shares: req.body.shares, 
+            price: req.body.price,
+            timestamp: req.body.timestamp
+        }, {
+            where: {
+                id: id
+            }
+        });
+
+        res.status(200).send(updateTrade);
+    }
+    catch{
+        res.status(404).send("Something went wrong!");
+    }
+})
+
+app.patch("/trades/:id", async(req, res)=>{
+    try{
+        var id = req.params.id;
+        var updateTrade = await Trade.update(req.body, {
+            where: {
+                id: id
+            },
+            individualHooks: true
+        });
+
+        res.status(200).send(updateTrade);
+    }
+    catch{
+        res.status(404).send("An Error occured!");
+    }
+})
+
+app.delete("/trades/:id", async(req, res)=>{
+    try{
+        var id = req.params.id;
+        await Trade.destroy(
+            {
+                where: {id: id}
+            },
+        );
+        res.status(200).send("Deleted!");
+    }
+    catch{
+        res.status(404).send("An Error occured!");
+    }
+})
+
+
+
 module.exports = app;
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
